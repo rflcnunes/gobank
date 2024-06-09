99d5a54289cfb46c783f04b6d70fac24889c9d07
Rafael <rafaelcnunes@icloud.com>
2024-06-09 03:38:09 -0300

    add withdraw

--- main.go
+++ main.go
@@ -9,6 +9,19 @@ type CheckingAccount struct {
 	balance       float64
 }
 
+func (account *CheckingAccount) Withdraw(amount float64) string {
+	var canWithdraw bool = account.balance < amount && amount > 0
+
+	if canWithdraw {
+		fmt.Println("Impossible to withdraw.")
+		return "Impossible to withdraw."
+	}
+
+	account.balance -= amount
+	fmt.Println("Withdrawal successful")
+	return "Withdrawal successful"
+}
+
 func main() {
 	johnsAccount := CheckingAccount{
 		"John Doe",
@@ -23,6 +36,10 @@ func main() {
 		5679,
 		238.43,
 	}
+
+	fmt.Println("Checking Accounts\n", johnsAccount, "\n", marysAccount)
+
+	johnsAccount.Withdraw(120.00)
 
 	fmt.Println("Checking Accounts\n", johnsAccount, "\n", marysAccount)
 }
